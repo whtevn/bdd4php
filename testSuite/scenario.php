ffc0d54b6edd4112735cc5742f5f474e9b9e0f0d
evan <evan@sdicg.com>
2013-12-03 19:50:38 -0500

    adding methods and functionality for pending tests

--- testSuite/scenario.php
+++ testSuite/scenario.php
@@ -4,9 +4,18 @@
 	include 'expectation.php';	
 	include 'reporter.php';	
 
+	function xexpect($val){
+		return Scenario::runExpectation($val, false);
+	}
+
+	function expect($val){
+		return Scenario::runExpectation($val);
+	}
+
 	final class Scenario {
 		private static $expectations=array();
 		private static $colors;
+		private static $rejected=false;
 		public static function when($title, $func) {
 			static::$colors =  new Colors();
 			$scene = new Scenario(); 
@@ -28,7 +37,9 @@
 		private static function reportOn($expectations){
 			$report = array('success'=>array(), 'failure'=>array());
 			foreach($expectations as $expectation){
-				if($expectation->success){
+				if($expectation->pending){
+					$report['pending'][] = $expectation;
+				}else if($expectation->success){
 					$report['success'][] = $expectation;
 				}else{
 					$report['failure'][] = $expectation;
@@ -54,11 +65,17 @@
 			return $this;
 		}
 
-		public function the($title, $func){
-			if(!is_callable('expect')){
-				function expect($val){
-					return Scenario::runExpectation(new Expectation($val));
-				}
+		public function xxthe($title, $func){
+			return $this;
+		}
+		public function xthe($title, $func){
+			return $this->the($title, $func, true);
+		}
+
+		public function the($title, $func, $dontdoit=false){
+			//set_error_handler('');
+			if($dontdoit){
+				static::$rejected = true;
 			}
 			foreach($this->doBeforeEach as $bfunc){
 				$bfunc($this);
@@ -67,10 +84,16 @@
 			foreach($this->doAfterEach as $bfunc){
 				$bfunc($this);
 			}
+			static::$rejected = false;
+			//restore_error_handler();
 			return $this;
 		}
 
-		public static function runExpectation($exp){
+		public static function runExpectation($exp, $doRun=true){
+			$pending = (!$doRun || static::$rejected);
+
+			$exp = new Expectation($exp, $pending);
+
 			static::$expectations[]=$exp;
 			return $exp;
 		}
